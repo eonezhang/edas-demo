0616c0af54c4040e5690b7798b2b4721fbcc57ba
eonezhang <eonezhang@qq.com>
2018-05-08 15:37:45 +0800

    DEV-DRIVEN

--- order-service/src/main/java/com/haiker/edas/demo/order/components/ConfigCenter.java
+++ order-service/src/main/java/com/haiker/edas/demo/order/components/ConfigCenter.java
@@ -1,23 +1,23 @@
-package com.haiker.edas.demo.order.components;
-
-import com.alibaba.edas.configcenter.config.ConfigChangeListener;
-import com.alibaba.edas.configcenter.config.ConfigService;
-
-/**
- * @author eonezhang 26/04/2018
- */
-public class ConfigCenter {
-    private static String config = "";
-
-    public static void initConfig() {
-
-        String dataId = "application";
-        String group = "order-service";
-        ConfigService.addListener(dataId, group, new ConfigChangeListener() {
-           public void receiveConfigInfo(String configInfo) {
-               config = configInfo;
-               System.out.printf("configInfo: %s\n", configInfo);
-           }
-        });
-    }
-}
+//package com.haiker.edas.demo.order.components;
+//
+//import com.alibaba.edas.configcenter.config.ConfigChangeListener;
+//import com.alibaba.edas.configcenter.config.ConfigService;
+//
+///**
+// * @author eonezhang 26/04/2018
+// */
+//public class ConfigCenter {
+//    private static String config = "";
+//
+//    public static void initConfig() {
+//
+//        String dataId = "application";
+//        String group = "order-service";
+//        ConfigService.addListener(dataId, group, new ConfigChangeListener() {
+//           public void receiveConfigInfo(String configInfo) {
+//               config = configInfo;
+//               System.out.printf("configInfo: %s\n", configInfo);
+//           }
+//        });
+//    }
+//}
